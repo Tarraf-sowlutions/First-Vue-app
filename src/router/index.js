d61df9bae65040c8a5bb03d88a6e1e96613013c6
Sowlutions <sowlutions@Tarrafs-MacBook-Pro.local>
2024-08-23 11:06:30 +0300

    adding 404 page 1.2

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import EventDetails from '../views/EventDetails.vue'
 import AboutView from '../views/AboutView.vue'
+import NotFoundView from '../views/NotFoundView.vue' // Import your 404 component
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +25,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: AboutView
+    },
+    {
+      path: '/:pathMatch(.*)*', // This is the catch-all route
+      name: 'not-found',
+      component: NotFoundView
     }
   ]
 })
